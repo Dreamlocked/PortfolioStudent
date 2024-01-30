ecbb1ed12a03eca287afe302b02218fc7c26db97
Leonardo Olaechea <71423514+Dreamlocked@users.noreply.github.com>
2024-01-30 05:50:02 +0000

    Main branch

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Leonardo Olaechea",
+  title: ".NET Developer",
+  email: "leonardo.olaechea@outlook.com",
+  gitHub: "dreamlocked",
+  instagram: "",
+  linkedIn: "leonardo-o-95b8b5140",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a .NET Developer with a passion for backend. I am always looking for new ways to make tech accessible to all. I am a creative problem solver who is always looking for new ways to drive innovation.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  ".NET Core",
+  "SQL SERVER",
+  "C#",
+  "JS",
+  "Python",
+  "Azure",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my developer experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
